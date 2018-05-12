59523b30f0c04b3408c9606eac96143a2d23a4af
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-05-12 12:32:19 +0700

    add test

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <NavbarHeader/>
-        {/* <header className="App-header"> */}
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1> */}
-        {/* </header> */}
         <Home />
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Enzyme from 'enzyme';
+import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import NavbarHeader from './components/NavbarHeader';
+import Home from './components/Home'
+import UserStore from './stores/UserStore'
+import { Provider } from 'mobx-react'
+import Modal from './components/general/Modal'
+import ModalAddList from './components/general/ModalAddList'
+import SearchFeature from './components/general/SearchFeature'
+import RegisterForm from "./components/general/RegisterForm"
+import LoginForm from "./components/general/LoginForm"
 
 Enzyme.configure({ adapter: new Adapter() })
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
-// describe('<App /> has NavbarHeader and Home Components')
+describe('<App /> rendering', () => {
+  it('should render navbar header component and home', () => {
+    const wrapper = shallow(<Provider UserStore={UserStore}><App /></Provider>)
+    expect(wrapper.containsAllMatchingElements([
+      <div />,
+      <NavbarHeader />,
+      <Home />
+    ]))
+  })
+})
+
+describe('<NavbarHeader /> render', () => {
+  it('should has component <Modal /> when it is not login', () => {
+    UserStore.isLogin = false
+    const wrapper = shallow(<nav />)
+    expect(wrapper.containsAllMatchingElements([
+      <div />,
+      <Modal />,
+    ]))
+    expect(wrapper.containsMatchingElement(<ModalAddList />)).toBeFalsy()
+    expect(wrapper.containsMatchingElement(<SearchFeature />)).toBeFalsy()
+  })
+
+  it('should has component <ModalAddList /> and <SeacrhFeature />', () => {
+    UserStore.isLogin = true
+    const wrapper = shallow(<nav />)
+    expect(wrapper.containsAllMatchingElements([
+      <ModalAddList />,
+      <SearchFeature />
+    ]))
+    expect(wrapper.containsMatchingElement(<Modal />)).toBeFalsy()
+  })
+})
+
+describe('<Modal /> render', () => {
+  it('should has component <RegisterForm /> and <LoginForm />', () => {
+    const wrapper = shallow(<div />)
+    expect(wrapper.containsAllMatchingElements([
+      <LoginForm />,
+      <RegisterForm />
+    ]))
+  })
+
+  it('login and register modal should has style display none before its called and display block after its called', () => {
+    const registerModal = shallow(<div id="myModal"/>)
+    const loginModal = shallow(<div id="loginModal"/>)
+    expect(registerModal.hasClass('scale-up-center'))
+    expect(registerModal.hasClass('modal'))
+    expect(loginModal.hasClass('modal'))
+    expect(loginModal.hasClass('scale-up-center'))
+  })
+})
