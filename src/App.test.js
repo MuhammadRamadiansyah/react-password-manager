7ded06837f683c7ecf8c5b95b17ea5a38a6ebf24
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-05-12 12:37:25 +0700

    add style test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'
 import App from './App';
 import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
